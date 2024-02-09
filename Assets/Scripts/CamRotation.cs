a29ff822f8005a7027fa6a080882823061579553
TimeForDevThat <antosha.07@mail.ru>
2024-02-09 14:20:01 +0500

    A lot of changes, better commit

---
+++ Assets/Scripts/CamRotation.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UIElements;
+
+public class CamRotation : MonoBehaviour
+{
+    public Transform CamAxisTransform;
+    public float RotationSpeed;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+     transform.localEulerAngles = new Vector3(0, transform.localEulerAngles.y + Time.deltaTime * RotationSpeed * Input.GetAxis("Mouse X"), 0);
+     CamAxisTransform.localEulerAngles = new Vector3(CamAxisTransform.localEulerAngles.x + Time.deltaTime * RotationSpeed * -Input.GetAxis("Mouse Y"), 0, 0);
+    }
+}
