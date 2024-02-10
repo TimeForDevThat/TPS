d6959b4129cb75e5600f9cee507c49e406b0da8f
TimeForDevThat <antosha.07@mail.ru>
2024-02-10 12:18:41 +0500

    Camera Rotation Finished

--- Assets/Scripts/CamRotation.cs
+++ Assets/Scripts/CamRotation.cs
@@ -5,6 +5,8 @@ public class CamRotation : MonoBehaviour
 {
     public Transform CamAxisTransform;
     public float RotationSpeed;
+    public float MinAngle;
+    public float MaxAngle;
     void Start()
     {
         
@@ -14,6 +16,10 @@ public class CamRotation : MonoBehaviour
     void Update()
     {
      transform.localEulerAngles = new Vector3(0, transform.localEulerAngles.y + Time.deltaTime * RotationSpeed * Input.GetAxis("Mouse X"), 0);
-     CamAxisTransform.localEulerAngles = new Vector3(CamAxisTransform.localEulerAngles.x + Time.deltaTime * RotationSpeed * -Input.GetAxis("Mouse Y"), 0, 0);
+     var NewAngleX = CamAxisTransform.localEulerAngles.x + Time.deltaTime * RotationSpeed * -Input.GetAxis("Mouse Y");
+     if (NewAngleX > 180)
+         NewAngleX -= 360;
+     NewAngleX = Mathf.Clamp(NewAngleX, MinAngle, MaxAngle);
+        CamAxisTransform.localEulerAngles = new Vector3(NewAngleX, 0, 0);
     }
 }
