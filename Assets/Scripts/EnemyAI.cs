d5f9ea71f5e29dccfc98f3863dac8a8724885980
TimeForDevThat <antosha.07@mail.ru>
2024-02-18 13:07:54 +0500

    Enemy Navigation AI

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -6,17 +6,20 @@ public class EnemyAI : MonoBehaviour
 {
     public List<Transform> PatrolPoints;
     private NavMeshAgent _navMeshAgent;
-    // Start is called before the first frame update
+    public PlayerController Player;
+    private bool _isPlayerGotNoticed;
+    public float ViewAngle;
     void Start()
     {
         InitComponentLinks();
         SelectPatrolPoint();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        PatrolUpdate();   
+        PatrolUpdate();
+        Raycast();
+        PlayerGotNoticed();
     }
     private void SelectPatrolPoint()
     {
@@ -24,13 +27,43 @@ public class EnemyAI : MonoBehaviour
     }
     private void PatrolUpdate()
     {
-        if (_navMeshAgent.remainingDistance == 0)
+        if(_navMeshAgent.remainingDistance == 0)
         {
-            SelectPatrolPoint();
+            if (_isPlayerGotNoticed == false)
+            {
+                SelectPatrolPoint();
+            }
         }
+
     }
     private void InitComponentLinks()
     {
         _navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+    private void Raycast()
+    {
+        var Direction = Player.transform.position - transform.position;
+        
+        _isPlayerGotNoticed = false;
+        if (Vector3.Angle(transform.forward, Direction)< ViewAngle)
+        {
+
+        
+            RaycastHit hit;
+            if (Physics.Raycast(transform.position + Vector3.up, Direction, out hit))
+            {
+                if (hit.collider.gameObject == Player.gameObject)
+                {
+                    _isPlayerGotNoticed = true;
+                }
+            }
+        }
+    }
+    private void PlayerGotNoticed()
+    {
+        if (_isPlayerGotNoticed)
+        {
+            _navMeshAgent.destination = Player.transform.position;
+        }
     }
 }
